dd4ba18aef34ea5a99b95f5135373d43fc92fd2c
keyhole0 <45264235+keyhole0@users.noreply.github.com>
2019-08-08 00:20:10 +0900

    多角形の衝突判定処理を追加（点と多角形の内外判定が未完成）

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ https://keyhole0.github.io/StaticOverlapDetection/overlapImage.html
 
 ## 多角形衝突判定サンプルページ
 https://keyhole0.github.io/StaticOverlapDetection/collisionPolygon.html
+
+## 参考資料
+
+線分交差判定  
+https://qiita.com/ykob/items/ab7f30c43a0ed52d16f2
+
+点と凹多角形の内外判定を行う
+http://edom18.hateblo.jp/entry/2018/11/28/200032
--- collisionPolygon.js
+++ collisionPolygon.js
@@ -4,6 +4,12 @@ class Vector{
         this.x = x;
         this.y = y;
     }
+    sub(vec){
+        return new Vector(this.x - vec.x, this.y - vec.y);
+    }
+    angle(){
+        return Math.atan2(this.y, this.x);
+    }
 }
 
 class Star{
@@ -21,6 +27,7 @@ class Star{
             rcnt += 1;
         }
         this.imagedata = null;
+        this.isHit = false;
     }
     setImageData(imagedata){
         this.imagedata = imagedata;
@@ -37,8 +44,13 @@ class Canvas{
         this.duplicateArray.fill(0);
     }
     
-    draw(points){
+    draw(points, isHit){
         this.ctx.beginPath();
+        if(isHit){
+            this.ctx.fillStyle = 'rgba(192, 80, 77, 0.5)';
+        }else{
+            this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        }
         this.ctx.moveTo(points[0].x, points[0].y);
         for(let i=0; i<points.length; ++i){
             this.ctx.lineTo(points[i].x, points[i].y); // 3.指定座標まで線を引く
@@ -70,10 +82,65 @@ class Canvas{
     }
 }
 
+//線分同士の交差判定
+function clossLine(ax, ay, bx, by, cx, cy, dx, dy){
+    var ta = (cx - dx) * (ay - cy) + (cy - dy) * (cx - ax);
+    var tb = (cx - dx) * (by - cy) + (cy - dy) * (cx - bx);
+    var tc = (ax - bx) * (cy - ay) + (ay - by) * (ax - cx);
+    var td = (ax - bx) * (dy - ay) + (ay - by) * (ax - dx);
+
+    //return tc * td < 0 && ta * tb < 0;
+    return tc * td <= 0 && ta * tb <= 0; // 端点を含む場合
+    //trueが返る場合は2つの線分が交差している。
+    //falseが返る場合は2つの線分が交差していない。
+}
+
+//点の内判定
+function pointInPolygon(point, polygon){
+    let extend = new Vector(point.x, 10000); //y軸方向に伸ばす（値は判定に影響の無い範囲で適当に決める）
+    let cnt = 0;
+    for(let i=0; i<polygon.length; ++i){
+        let vs1 = polygon[i];
+        let ve1 = polygon[(i+1)%polygon.length];
+        if(clossLine(vs1.x, vs1.y, ve1.x, ve1.y, point.x, point.y, extend.x, extend.y)){
+            cnt += 1;
+        }
+    }
+    //ポリゴンの頂点と交差しているとき、２つの辺と交差している扱いとなってしまっているため、正しく判定できていない。
+    //点から一方向に伸ばした線分とポリゴンが交差している数が奇数ならポリゴンの内側になる
+    return cnt % 2 == 1;
+}
+
+//ポリゴン同士の当たり判定
+function hitPolygon(poly1, poly2){
+    //辺の交差判定
+    for(let i=0; i<poly1.length; ++i){
+        for(let j=0; j<poly2.length; ++j){
+            let vs1 = poly1[i];
+            let ve1 = poly1[(i+1)%poly1.length];
+            let vs2 = poly2[j];
+            let ve2 = poly2[(j+1)%poly2.length];
+            if(clossLine(vs1.x, vs1.y, ve1.x, ve1.y, vs2.x, vs2.y, ve2.x, ve2.y)){
+                return true;
+            }
+        }
+    }
+    
+    //点の内外判定（辺の交差に引っかからないパターンは一方がもう一方を完全に内包しているパターンしか無いため、一つの点について判定すれば十分
+    if(pointInPolygon(poly1[1], poly2)){
+        return true;
+    }
+    if(pointInPolygon(poly2[1], poly1)){
+        return true;
+    }
+
+    return false;
+}
+
 function main(){
 
     const startTime = performance.now();
-    let objectNum = 1000;
+    let objectNum = 0;
 
     let canvas = new Canvas('cv');
 
@@ -86,15 +153,25 @@ function main(){
         stars.push(new Star(x, y, r, a));
     }
 
+    //以下は辺が交差しないがヒットしているサンプルのオブジェクト
+    stars.push(new Star(350, 250, 50, -90));
+    stars.push(new Star(350, 250, 10, -90));
+    //stars.push(new Star(550, 250, 10, -90));
+    //stars.push(new Star(550, 250, 25, -90));
+
+    //総当り
     for(let i=0; i<stars.length; ++i){
-        canvas.drawImageData(stars[i].points);
+        for(let j=i+1; j<stars.length; ++j){
+            if(hitPolygon(stars[i].points, stars[j].points)){
+                stars[i].isHit = true;
+                stars[j].isHit = true;
+            }
+        }
     }
     
     for(let i=0; i<stars.length; ++i){
-        canvas.draw(stars[i].points);
+        canvas.draw(stars[i].points, stars[i].isHit);
     }
-    
-    canvas.hitImageData(stars);
 
     const endTime = performance.now();
     let performanceTime = Math.round((endTime - startTime)*1000)/1000
